f88f114975af6adb91bbfa7a607d451b6d98d097
Adriean Khisbe <adriean.khisbe@live.fr>
2013-12-11 11:54:42 +0100

    Clean up après soutenance

--- src/core/asHuffman.java
+++ src/core/asHuffman.java
@@ -1,6 +1,5 @@
 package core;
 
-
 import java.io.IOException;
 
 import tools.Stats;
@@ -12,26 +11,16 @@ public class asHuffman {
 		final String USAGE = "Usage: (de)compression inputfile outputfile";
 		// Stéphane, uuun throws ici....
 
-		/*
-		 * Options jct = new Options(); JCommander jCommander = null; String[]
-		 * argv = { "-arg", "1", "test", "test" };
-		 * 
-		 * try { jCommander = new JCommander(jct, argv); } catch
-		 * (com.beust.jcommander.ParameterException e) { System.out.println(e);
-		 * String[] ex = { "-arg", "1", "test" }; JCommander exe = new
-		 * JCommander(jct, ex); exe.usage(); }
-		 */
-		
-		//TODO: desactiver après tests...
+		// TODO: desactiver après tests...
 		if (args.length == 0) {
 			System.out.println("Lancement en mode test");
 			testMode("fichier_original.txt", "fichier_compresse.dat",
 					"fichier_decompresse.txt");
 			System.out.println("Fin du test");
-			
+
 			// System.err.println(USAGE);
 			// System.exit(1);
-			
+
 		} else if (args.length == 3) {
 
 			// TODO: argument checking!!
@@ -43,14 +32,15 @@ public class asHuffman {
 				System.out.println("Lancement AsHuffman en mode compression");
 				compressMode(args[1], args[2]);
 				break;
-				
+
 			case "d":
 			case "decompression":
 				System.out.println("Lancement AsHuffman en mode decompression");
 				decompressMode(args[1], args[2]);
 				break;
 
-			case "test": case "t":
+			case "test":
+			case "t":
 				System.out.println("Lancement AsHuffman en mode test");
 				testMode(args[1], args[2]);
 				break;
@@ -61,16 +51,9 @@ public class asHuffman {
 			}
 
 		} else {
-			System.err.println((args[0].equals(egmp))? eegg:USAGE);
+			System.err.println((args[0].equals(egmp)) ? eegg : USAGE);
 			System.exit(1);
 		}
-
-		/*
-		 * // TODO: vite changer cette ....Belle chose :) switch (jct.getOpt())
-		 * { case 1: break; case 2: break; case 3: break; default:
-		 * System.out.println("Mauvais numéro utilisé en argument \"arg\"");
-		 * jCommander.usage(); break; }
-		 */
 
 	}
 
@@ -81,22 +64,21 @@ public class asHuffman {
 		System.out.println(">> Compression du fichier " + inputFile + " vers "
 				+ compressedFile);
 
-		
-		
 		Stats.lanceChrono();
 		Compresseur c = new Compresseur(inputFile, compressedFile);
 		c.compression();
 
 		Stats.stopChrono();
-		 Stats.printChrono("compression");
-		//Stats.printDebug("Fin Compression");
-		//Stats.printCompressionWriteStats();
-		//Stats.printStats();
-		 
-		 float tauxCompression  = Stats.compareFileSize(inputFile, compressedFile);
-		 System.out.println("Taux de compression: "+ (100 -(tauxCompression *  100)) +"%");
-		 
-	
+		Stats.printChrono("compression");
+		// §DISStats.printDebug("Fin Compression");
+		// §DISStats.printCompressionWriteStats();
+		// §DISStats.printStats();
+
+		float tauxCompression = Stats
+				.compareFileSize(inputFile, compressedFile);
+		System.out.println("Taux de compression: "
+				+ (100 - (tauxCompression * 100)) + "%");
+
 	}
 
 	private static void decompressMode(String compressedFile, String outPutFile)
@@ -104,13 +86,13 @@ public class asHuffman {
 		System.out.println(">> DeCompression du fichier " + compressedFile
 				+ " vers " + outPutFile);
 
-		//§DISStats.printDebug("Début Decompression");
+		// §DISStats.printDebug("Début Decompression");
 		Stats.lanceChrono();
 		Decompresseur d = new Decompresseur(compressedFile, outPutFile);
 		d.decompression();
 		Stats.stopChrono();
 		Stats.printChrono("decompression");
-		//Stats.printStats();
+		// Stats.printStats();
 	}
 
 	private static void testMode(String inputFile, String compressedFile,
@@ -125,7 +107,8 @@ public class asHuffman {
 		testMode(inputFile, "/tmp/compress", outPutFile);
 		// marchera que sur unix.
 	}
-	
-	private static String egmp= "troll", eegg="Attention, coeur application conçu par un Clown, bozo";
-	
+
+	private static String egmp = "troll",
+			eegg = "Attention, coeur application conçu par un Clown, bozo";
+
 }
